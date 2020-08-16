df892d1ebb4da323c68515b239a4bb592d53bf87
Rodri <rodrigo-1905@hotmail.com>
2020-08-16 18:44:40 -0300

    Agrego codigo a main.js

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 class InterfazDeUsuario{
 
 	agregarGasto(gasto){
-		const listaGastos = document.getElementById("");
+		const listaGastos = document.getElementById("listaGastos");
+		const nuevoGasto = document.createElement("li");
+		nuevoGasto.setAttribute("class", "item");
+		const contenido = `
+			<p class="ingreso-datos__datos__b">Titulo: ${gasto.titulo}</p>
+			<p class="ingreso-datos__datos__b">Descripcion: ${gasto.descripcion}</p>
+			<p class="ingreso-datos__datos__b">Precio: $${gasto.precio}</p>
+		`;
 
-
+		nuevoGasto.innerHTML = contenido;
+		listaGastos.appendChild(nuevoGasto);
 	}
 
 	borrarGasto(){
@@ -30,7 +38,9 @@ document.getElementById('formulario').addEventListener('submit', function(e){
 	let precio = document.getElementById("precio").value;
 
 	const gasto = new Gastos(titulo, descripcion, precio);
-
 	console.log(gasto);
+
+	const UI = new InterfazDeUsuario();
+	UI.agregarGasto(gasto);
 
 });
